8fc9a4f57d9cebc3bb52a7ec0757f2c85d3d0167
boblamceo <boblam.ceo@gmail.com>
2024-11-27 16:18:00 +0800

    put map

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,91 @@
-import Image from "next/image";
+"use client";
+
+import axios from "axios";
+import {
+    MapContainer,
+    Marker,
+    Popup,
+    TileLayer,
+    useMapEvents,
+    ZoomControl,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
+import L from "leaflet";
+
+const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
+
+function DetectClick({ setCoordinate }) {
+    useMapEvents({
+        click: (e) => {
+            setCoordinate([e.latlng.lat, e.latlng.lng]);
+        },
+    });
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [coordinate, setCoordinate] = useState([null, null]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    useEffect(() => {
+        console.log(coordinate);
+        if (typeof coordinate[0] === "number") {
+            console.log("before");
+            axios
+                .get(
+                    `https://isitwater-com.p.rapidapi.com/?latitude=${coordinate[0]}&longitude=${coordinate[1]}`,
+                    {
+                        headers: {
+                            "x-rapidapi-key":
+                                "1ca6713b92mshe945944449df0d5p115855jsn8f911baafb57",
+                            "x-rapidapi-host": "isitwater-com.p.rapidapi.com",
+                        },
+                    }
+                )
+                .then((res) => {
+                    console.log("done");
+                    const isWater = res.data.water;
+
+                    if (isWater) {
+                        alert("that is on water");
+                    } else {
+                        alert("Not on water!!!");
+                    }
+                });
+        }
+    }, [coordinate]);
+    return (
+        <div className="w-screen h-screen">
+            <div className="text-white title text-[6vw] w-[60vw] absolute z-[100000]">
+                Plastic Tracker
+            </div>
+            <div className="w-[40vw] h-[40vh] bg-black p-[1vw] z-[1000] fixed">
+                <div className="text-white top-[8vw] absolute fade-in text-[1.5vw] w-[35vw]">
+                    This is a free tool to see where your plastic goes! Click on
+                    the map to see where a piece of plastic will end up after 1
+                    month.
+                </div>
+            </div>
+
+            <MapContainer
+                center={[0, 0]}
+                zoom={1}
+                className="w-screen h-screen fade-in"
+                zoomControl={false}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <ZoomControl position="bottomright" />
+                <DetectClick setCoordinate={setCoordinate} />
+                {typeof coordinate[0] === "number" && (
+                    <Marker position={coordinate} icon={icon}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                )}
+            </MapContainer>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
